4f0bcde1e553774820e3bb01a4ed5f5f3a9a9b33
Gustavo Oliveira <148923375+GustavoOliveiraDr@users.noreply.github.com>
2025-04-07 21:52:51 -0300

    Adiciona função para buscar clima via API

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
 # App de clima - versão inicial
+
+import requests
+
+API_KEY = 'SUA_API_KEY_AQUI'
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
+
+def pegar_clima(cidade):
+    params = {
+        "q": cidade,
+        "appid": API_KEY,
+        "lang": "pt_br",
+        "units": "metric"
+
+    }
+
+    resposta = resquests.get(BASE_URL, params=params)
+    dados = resposta.json()
+    return dados

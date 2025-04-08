0aab1b8d66db0ac44d658a1ed96c1ab7f475072b
Gustavo Oliveira <148923375+GustavoOliveiraDr@users.noreply.github.com>
2025-04-07 21:59:41 -0300

    Melhora exibição dos dados no terminal com formtação

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-# App de clima - versão inicial
-
 import requests
 
-API_KEY = 'SUA_API_KEY_AQUI'
+API_KEY = "SUA_API_KEY_AQUI"
 BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
+
 
 def pegar_clima(cidade):
     params = {
@@ -11,18 +10,25 @@ def pegar_clima(cidade):
         "appid": API_KEY,
         "lang": "pt_br",
         "units": "metric"
-
     }
 
-    resposta = resquests.get(BASE_URL, params=params)
-    dados = resposta.json()
-    return dados
+    resposta = requests.get(BASE_URL, params=params)
 
-#entrada do usuário
+    if resposta.status_code == 200:
+        return resposta.json()
+    else:
+        return None
+
+
+# Entrada do usuário
 cidade = input("Digite o nome da cidade: ")
 dados = pegar_clima(cidade)
 
 if dados:
-    print(dados)
+    print(f"\n Clima em {cidade.title()}:")
+    print(f" Temperatura: {dados['main']['temp']}°C")
+    print(f" Sensação térmica: {dados['main']['feels_like']}°C")
+    print(f" Umidade: {dados['main']['humidity']}%")
+    print(f" ️ Condição: {dados['weather'][0]['description'].capitalize()}")
 else:
-    print("Cidade não encontrada ou erro na requisição")
+    print("❌ Cidade não encontrada ou erro na requisição.")

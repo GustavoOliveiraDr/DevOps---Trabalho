0a6d3579b4399bc6cd353cbb7951a652b0a91655
Gustavo Oliveira <148923375+GustavoOliveiraDr@users.noreply.github.com>
2025-04-28 19:30:55 -0300

    Ajustando alguns erros #4

--- src/app.py
+++ src/app.py
@@ -22,18 +22,21 @@ def pegar_clima(city):
         return None
 
 
-# Entrada do usuário
-cidade = input("Digite o nome da cidade: ")
-dados = pegar_clima(cidade)
+def main():
+    # Entrada do usuário
+    cidade = input("Digite o nome da cidade: ")
+    dados = pegar_clima(cidade)
 
-if dados:
-    print(f"\n Clima em {cidade.title()}:")
-    print(f" Temperatura: {dados['main']['temp']}°C")
-    print(f" Sensação térmica: {dados['main']['feels_like']}°C")
-    print(f" Umidade: {dados['main']['humidity']}%")
-    print(f" ️ Condição: {dados['weather'][0]['description'].capitalize()}")
-else:
-    print("❌ Cidade não encontrada ou erro na requisição.")
-    print("Ahhh Não deu certo")
-    #alguma coisa ai por enquanto
+    if dados:
+        print(f"\n Clima em {cidade.title()}:")
+        print(f" Temperatura: {dados['main']['temp']}°C")
+        print(f" Sensação térmica: {dados['main']['feels_like']}°C")
+        print(f" Umidade: {dados['main']['humidity']}%")
+        print(f" ️ Condição: {dados['weather'][0]['description'].capitalize()}")
+    else:
+        print("❌ Cidade não encontrada ou erro na requisição.")
+        print("Ahhh Não deu certo")
 
+
+if __name__ == "__main__":
+    main()
